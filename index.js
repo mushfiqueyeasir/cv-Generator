dc011d76db6eec58512dc5cbd6105a0b5b7f9b18
Mushfique Yeasir <92722825+mushfiqueyeasir@users.noreply.github.com>
2022-09-01 04:41:55 +0600

    jwt removed'

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
@@ -16,23 +15,6 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@clusterprime.emiebxm.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-// Java Web Token Verify
-function verifyJWT(req, res, next) {
-
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-        return res.status(401).send({ message: 'UnAuthorized access' });
-    }
-    const token = authHeader.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-        if (err) {
-            return res.status(403).send({ message: 'Forbidden access!' })
-        }
-        req.decoded = decoded;
-        next();
-    })
-}
 
 
 async function run(){
@@ -51,7 +33,7 @@ async function run(){
         })
 
         // Get All UserInformation
-        app.get('/',verifyJWT, async(req,res)=>{
+        app.get('/', async(req,res)=>{
             const query ={};
             const cursor = userInformationCollection.find(query);
             const userInformation =  await cursor.toArray();
@@ -69,8 +51,7 @@ async function run(){
                 $set: user,
             };
             const result = await userInformationCollection.updateOne(filter, updateDoc, options);
-            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '12h' })
-            res.send({result,token});
+            res.send(result);
         })
     }
     finally{
